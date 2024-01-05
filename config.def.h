d8ac98fd095202db9160cd96c8200cd22816d050
p4p1 <p4p1@vivaldi.net>
2024-01-05 23:53:30 +0100

    better st

--- config.def.h
+++ config.def.h
@@ -291,7 +291,7 @@ static Key keys[] = {
 	/***** Stealing these keys for the pine phone port ****/
 	{ 0, XF86XK_AudioLowerVolume, openkeyboard, {0} },
 	{ 0, XF86XK_AudioRaiseVolume, spawn, {.v = roficmd } },
-	{ 0, XF86XK_PowerOff, spawn, { .v =  termcmd  } },
+	{ 0, XF86XK_PowerOff, spawn, { .v =  termcmd_tmux  } },
 	/***** Stealing these keys for the pine phone port ****/
 
 	{ 0, XF86XK_MonBrightnessUp, spawn, {.v = monMax} },

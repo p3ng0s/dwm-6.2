900d1add6e9204c939ab6ea5ec15bf90f034fc18
p4p1 <p4piiii@proton.me>
2026-03-08 22:00:27 +0100

    I guess no more caps for me^^

--- config.def.h
+++ config.def.h
@@ -50,7 +50,7 @@ static const char mon_name[]        = "eDP-1";
 
 // Autostart information
 static unsigned int child_pid = 0;
-static const char *(stup_prgs[16][7])    = {   // [Number_programs][Number_args]
+static const char *(stup_prgs[15][7])    = {   // [Number_programs][Number_args]
 	/* prog path            arg1           arg2                          arg... */
 # if defined(_LIVE_MODE_)
 	{ "picom",              "--config",    "~/.config/picom/picom-vm.conf",                         NULL,   NULL,    NULL, NULL },
@@ -60,7 +60,6 @@ static const char *(stup_prgs[16][7])    = {   // [Number_programs][Number_args]
 	{ "blueman-applet",     NULL,          NULL,                         NULL,   NULL,    NULL, NULL },
 	{ "bash",               "-c",          "bash /etc/p3ng0s/display.sh",     NULL,   NULL,    NULL, NULL },
 	{ "bash",               "-c",          "feh --bg-fill $HOME/.wallpaper.png", NULL,   NULL,    NULL, NULL },
-	{ "setxkbmap",          "-option",     "caps:escape",                NULL,   NULL,    NULL, NULL },
 	{ "bash",               "-c",          "bash /etc/p3ng0s/bar.sh",     NULL,   NULL,    NULL, NULL },
 	{ "/usr/bin/xss-lock",  "--",          "/etc/p3ng0s/locker.sh",      NULL, NULL,  NULL },
 	{ "/usr/lib/notification-daemon-1.0/notification-daemon",       NULL, NULL, NULL, NULL, NULL, NULL },
